c84b27b61563790d3a6ea767e3f24ea3a564fe3a
Diar Xharavina <diarxharavina@gmail.com>
2024-07-13 14:11:07 +0200

    small change

--- server/database.sql
+++ server/database.sql
@@ -5,7 +5,7 @@ CREATE TABLE user(
     username VARCHAR(20) UNIQUE NOT NULL,
     password VARCHAR(20) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL
-)
+);
 
 CREATE TABLE song(
     song_id SERIAL PRIMARY KEY,
@@ -16,7 +16,7 @@ CREATE TABLE song(
     file_path VARCHAR(255),
     album_id INT,
     FOREIGN KEY (album_id) REFERENCES album(album_id)
-)
+);
 
 CREATE TABLE album(
     album_id SERIAL PRIMARY KEY,
@@ -24,7 +24,7 @@ CREATE TABLE album(
     artist VARCHAR(70),
     release_date DATE,
     cover_image_path VARCHAR(255)
-)
+);
     
 CREATE TABLE playlist(
     playlist_id SERIAL PRIMARY KEY,
@@ -33,18 +33,18 @@ CREATE TABLE playlist(
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
+CREATE TABLE playlist_item(
+    playlist_item_id SERIAL PRIMARY KEY,
+    playlist_id INT,
+    song_id INT,
+    FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id),
+    FOREIGN KEY (song_id) REFERENCES song(song_id)
+);
+
 CREATE TABLE like(
     like_id SERIAL PRIMARY KEY,
     user_id INT,
     audio_id INT,
     FOREIGN KEY (user_id) REFERENCES user(user_id),
-    FOREIGN KEY (song_id) REFERENCES song(song_id)
-);
-
-CREATE TABLE playlist_item(
-    playlist_item_id SERIAL PRIMARY KEY,
-    playlist_id INT,
-    song_id INT,
-    FOREIGN KEY (playlist_id) REFERENCES Playlists(playlist_id),
     FOREIGN KEY (song_id) REFERENCES song(song_id)
 );
